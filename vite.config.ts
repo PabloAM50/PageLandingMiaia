69e4cd339dfee11ba659839bc923ee24f60befc4
PabloAM50 <pabloracer50@hotmail.com>
2025-05-02 23:23:19 +0200

    vite changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Asegura que las rutas a los assets sean relativas a la raíz del dominio
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
